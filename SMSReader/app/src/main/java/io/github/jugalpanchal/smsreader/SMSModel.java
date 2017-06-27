8969412ff8265eaece11055931fc33fd8d691584
Jugal Panchal <jugalpanchal@gmail.com>
2017-06-27 09:02:09 +0530

    Declared sms model class

---
+++ SMSReader/app/src/main/java/io/github/jugalpanchal/smsreader/SMSModel.java
@@ -0,0 +1,27 @@
+package io.github.jugalpanchal.smsreader;
+
+/**
+ * Created by Jugal on 27-06-2017.
+ */
+
+public class SMSModel {
+
+    private String number;
+    private String text;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
