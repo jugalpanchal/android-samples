8cca3e9bc23f4cb755308874d85c3cc2856783ff
Jugal Panchal <jugalpanchal@gmail.com>
2017-07-03 09:03:06 +0530

    Show notification

--- SMSReader/app/src/main/java/io/github/jugalpanchal/smsreader/MainActivity.java
+++ SMSReader/app/src/main/java/io/github/jugalpanchal/smsreader/MainActivity.java
@@ -1,9 +1,16 @@
 package io.github.jugalpanchal.smsreader;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,11 +24,11 @@ public class MainActivity extends AppCompatActivity {
 
         List<SMSModel> smsList = new ArrayList<>();
         Uri uri = Uri.parse("content://sms/inbox");
-        Cursor cursor= getContentResolver().query(uri, null, null ,null,null);
+        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
         startManagingCursor(cursor);
 
-        if(cursor.moveToFirst()) {
-            for(int i=0; i < cursor.getCount(); i++) {
+        if (cursor.moveToFirst()) {
+            for (int i = 0; i < cursor.getCount(); i++) {
                 SMSModel sms = new SMSModel();
                 sms.setNumber(cursor.getString(cursor.getColumnIndexOrThrow("address")).toString());
                 sms.setText(cursor.getString(cursor.getColumnIndexOrThrow("body")).toString());
@@ -30,6 +37,29 @@ public class MainActivity extends AppCompatActivity {
                 cursor.moveToNext();
             }
         }
+
+        Toast.makeText(getApplicationContext(), smsList.toString(), Toast.LENGTH_LONG).show();
+
         cursor.close();
+        showNotification();
+    }
+
+    private void showNotification() {
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.notification_icon_background)
+                        .setContentTitle("SMS Reader")
+                        .setContentText("Waiting for SMS!");
+        Intent resultIntent = new Intent(this, MainActivity.class);
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(MainActivity.class);
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(1, mBuilder.build());
     }
 }
